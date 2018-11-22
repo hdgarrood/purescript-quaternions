32fd165e50f37b9eb18aedbc57b746bb38b8ed8f
Harry Garrood <harry@garrood.me>
2018-11-22 14:49:20 +0000

    Add vzero, vadd, vsub, scalarMul, Monoid for Vec3

--- src/Data/Quaternion/Vec3.purs
+++ src/Data/Quaternion/Vec3.purs
@@ -1,7 +1,18 @@
 -- | A minimal type for vectors in 3D space. This type is a newtype over `Array
 -- | a`, with constructors to ensure that we do in fact have exactly three
 -- | elements.
-module Data.Quaternion.Vec3 (Vec3, vec3, toArray, fromArray, normalize) where
+module Data.Quaternion.Vec3
+  ( Vec3
+  , vec3
+  , toArray
+  , fromArray
+  , normalize
+  , magnitude
+  , vzero
+  , vadd
+  , vsub
+  , scalarMul
+  ) where
 
 import Prelude
 
@@ -16,6 +27,12 @@ derive instance ordVec3 :: Ord a => Ord (Vec3 a)
 instance showVec3 :: Show a => Show (Vec3 a) where
   show (Vec3 v) = show v
 
+instance semigroupVec3 :: Semiring a => Semigroup (Vec3 a) where
+  append = vadd
+
+instance monoidVec3 :: Semiring a => Monoid (Vec3 a) where
+  mempty = vzero
+
 -- | Construct a `Vec3` from three arguments.
 vec3 :: forall a. a -> a -> a -> Vec3 a
 vec3 x y z = Vec3 [x, y, z]
@@ -29,14 +46,34 @@ toArray (Vec3 v) = v
 fromArray :: forall a. Partial => Array a -> Vec3 a
 fromArray = Vec3
 
+vzero :: forall a. Semiring a => Vec3 a
+vzero = Vec3 [zero, zero, zero]
+
+vadd :: forall a. Semiring a => Vec3 a -> Vec3 a -> Vec3 a
+vadd (Vec3 [x1, y1 ,z1]) (Vec3 [x2, y2, z2]) =
+  Vec3 [x1 + x2, y1 + y2, z1 + z2]
+vadd _ _ =
+  unsafeCrashWith "Vec3 invariant violated"
+
+vsub :: forall a. Ring a => Vec3 a -> Vec3 a -> Vec3 a
+vsub (Vec3 [x1, y1, z1]) (Vec3 [x2, y2, z2]) =
+  Vec3 [x1 - x2, y1 - y2, z1 - z2]
+vsub _ _ =
+  unsafeCrashWith "Vec3 invariant violated"
+
+scalarMul :: forall a. Semiring a => a -> Vec3 a -> Vec3 a
+scalarMul k (Vec3 [x,y,z]) =
+  Vec3 [k*x, k*y, k*z]
+scalarMul _ _ =
+  unsafeCrashWith "Vec3 invariant violated"
+
 -- | Normalize a vector, returning a unit vector pointing in the same
 -- | direction. Attempting to normalize the zero vector simply returns the zero
 -- | vector.
 normalize :: Vec3 Number -> Vec3 Number
-normalize (Vec3 [x,y,z]) =
-  let
-    k = Math.sqrt (x*x + y*y + z*z)
-  in
-    Vec3 [k*x, k*y, k*z]
-normalize _ =
-  unsafeCrashWith "Vec3 invariant violated"
+normalize v =
+  scalarMul (1.0 / magnitude v) v
+
+magnitude :: Vec3 Number -> Number
+magnitude (Vec3 [x,y,z]) = Math.sqrt (x*x + y*y + z*z)
+magnitude _ = unsafeCrashWith "Vec3 invariant violated"
